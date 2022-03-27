006b0f161fac0b5aefc3387b958e4da0b3622eb0
Gino Ureta <ing.gino@gmail.com>
2022-03-27 19:23:55 -0400

    Practice directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'directive-practice';
+  isDetailsVisible = false;
+  clicksArray = [];
+  counter = 1;
+
+  showDetails() {
+    this.isDetailsVisible = !this.isDetailsVisible;
+    // this.clicksArray.push(this.counter++);
+    this.clicksArray.push(new Date());
+  }
+
+  getColor(number) {
+    return number > 4 ? 'blue' : 'white';
+  }
 }
